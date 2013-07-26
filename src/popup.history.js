0c0d26bcaed8f5446cbd92db09cb3b2e9de24b7c
joeylin <joe.y.lin.2012@gmail.com>
2013-07-26 14:44:14 -0700

    clean files

--- src/popup.history.js
+++ src/popup.history.js
@@ -1,86 +1,85 @@
-// history plugin
-// fix me
-// (function(undefined) {
+history plugin
+fix me
+(function(undefined) {
 
-//     var hash = window.location.hash;
-//     var popId = 0;
-//     var hashEmit = true;
-//     var current = null;
-//     // for history
-//     function parseHash(hash) {
-//         var arr = hash.split('/');
-//         return {
-//             id: hash.replace('#',''),
-//             index: parseInt(arr[2]) || 0
-//         }
-//     }
+    var hash = window.location.hash;
+    var popId = 0;
+    var hashEmit = true;
+    var current = null;
+    // for history
+    function parseHash(hash) {
+        var arr = hash.split('/');
+        return {
+            id: hash.replace('#',''),
+            index: parseInt(arr[2]) || 0
+        }
+    }
 
-//     this.$element.each(function(i,v) {
-//         var name = $(v).data('popup-group'),
-//             index = i;
+    this.$element.each(function(i,v) {
+        var name = $(v).data('popup-group'),
+            index = i;
 
-//         if (!name) {
-//             popId++;
-//             name = popId;
-//         }
+        if (!name) {
+            popId++;
+            name = popId;
+        }
 
-//         $(v).attr('popup-id','/popup-' + name + '/' + index);
-//     });
+        $(v).attr('popup-id','/popup-' + name + '/' + index);
+    });
 
-//     // hash close
-//     hashEmit = false;
-//     window.location.hash = '';
-//     setTimeout(function() {
-//         hashEmit = true;
-//     }, 0);
+    // hash close
+    hashEmit = false;
+    window.location.hash = '/';
+    setTimeout(function() {
+        hashEmit = true;
+    }, 0);
 
-//     // history
-//     fixme: control hasEmit value accuratly, add to event
-//     hashEmit = false;
-//     window.location.hash = '#' + $(item.target).attr('popup-Id');
-//     setTimeout(function() {
-//         hashEmit = true;
-//     }, 0);
+    // history
+    //fixme: control hasEmit value accuratly, add to event
+    hashEmit = false;
+    window.location.hash = '#' + $(item.target).attr('popup-Id');
+    setTimeout(function() {
+        hashEmit = true;
+    }, 0);
 
-//     // open hash when use browser navigate
-//     $(window).on('hashchange.popup', function() {
+    // open hash when use browser navigate
+    $(window).on('hashchange.popup', function() {
+        if (!hashEmit) {
+            return false;
+        }
 
-//         if (!hashEmit) {
-//             return false;
-//         }
+        var hash = window.location.hash,
+            result = parseHash(hash),
+            $element = $('[popup-id="'+ result.id +'"]'),
+            instance = $element.data('popup'); 
 
-//         var hash = window.location.hash,
-//             result = parseHash(hash),
-//             $element = $('[popup-id="'+ result.id +'"]'),
-//             instance = $element.data('popup'); 
+        if (!instance) {
+            if (current) {
+                current.close();
+            }
+            return false;
+        }
 
-//         if (!instance) {
-//             if (current) {
-//                 current.close();
-//             }
-//             return false;
-//         }
+        if (instance.active === true) {
+            instance.goto(result.index);
+        } else {
+            $element.click();
+        }
+    });
 
-//         if (instance.active === true) {
-//             instance.goto(result.index);
-//         } else {
-//             $element.click();
-//         }
-//     });
+    fixme: 'popup' use namespace
+    if ($.type(hash) === 'string' &&  hash.match(/(popup)/i)) {
+        var result = parseHash(hash);
 
-//     // fixme: 'popup' use namespace
-//     // if ($.type(hash) === 'string' &&  hash.match(/(popup)/i)) {
-//     //     var result = parseHash(hash);
+        setTimeout(function() {
+            var $element = $('[popup-Id="' + result.id +'"]'),
+                instance = $element.data('popup');
 
-//     //     setTimeout(function() {
-//     //         var $element = $('[popup-Id="' + result.id +'"]'),
-//     //             instance = $element.data('popup');
-
-//     //         if (instance.active === true) {
-//     //             instance.goto(result.index);
-//     //         } else {
-//     //             $element.click();
-//     //         }
-//     //     }, 17);
-//     // }
-// })();
+            if (instance.active === true) {
+                instance.goto(result.index);
+            } else {
+                $element.click();
+            }
+        }, 17);
+    }
+})();
